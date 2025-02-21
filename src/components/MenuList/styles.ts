5a172d3f91d83ec2914b9a04c247b8eb8fb0c5e2
ademirsoares <ademir@licitaronline.com>
2025-02-21 09:09:28 -0300

    ajuste responsividade mobile 2

--- src/components/MenuList/styles.ts
+++ src/components/MenuList/styles.ts
@@ -6,10 +6,6 @@ export const Container = styled.section`
   display: flex;
   align-items: center;
   justify-content: center;
-
-  @media (max-width: ${breakpoints.desktop}) {
-    width: 100%;
-  }
 `
 
 export const List = styled.ul`
